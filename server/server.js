647eeda1c1056027e312a58a4a6bc7359bb16dac
Radha <radha.sable25@gmail.com>
2018-06-06 11:06:02 +0530

    Add GET /todos/:ids

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 // var mongoose = require('mongoose');
 var express = require('express');
 var bodyparser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {todo} = require('./models/todo');
 var {user} = require('./models/users');
@@ -39,6 +39,22 @@ app.get('/todos',(req,res) =>{
   });
 });
 
+//pass id to url GET /todos/12847t7t7e99ww453556
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
